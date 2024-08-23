a05bf6e184ae781467b8962d785d593f5f99808d
Anton <afeklichev@yandex.ru>
2024-08-23 18:20:00 +0300

    init commit

---
+++ src/test/java/antonfeklichev/task_tracker_application/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package antonfeklichev.task_tracker_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
