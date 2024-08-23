1d121b2fc0420b66d0fccdf57f8fb618bbcc38c6
Anton <afeklichev@yandex.ru>
2024-08-23 21:27:52 +0300

    add task and subtask controllers

--- src/main/java/antonfeklichev/tasktrackerapp/controller/TaskController.java
+++ src/main/java/antonfeklichev/tasktrackerapp/controller/TaskController.java
@@ -1,17 +1,54 @@
 package antonfeklichev.tasktrackerapp.controller;
 
+import antonfeklichev.tasktrackerapp.dto.NewTaskDto;
+import antonfeklichev.tasktrackerapp.dto.TaskDto;
 import antonfeklichev.tasktrackerapp.service.TaskService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/task")
+@RequestMapping(path = "api/v1/task")
 @RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskDto> addTask(@RequestBody NewTaskDto newTaskDto) {
+        return ResponseEntity.ok()
+                .body(taskService.addTask(newTaskDto));
+    }
+
+    @GetMapping(path = "/{taskId}")
+    public ResponseEntity<TaskDto> getTaskById(@PathVariable(name = "taskId") Long taskId) {
+        return ResponseEntity.ok().body(taskService.getTaskById(taskId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDto>> getAllTasks() {
+        return ResponseEntity.ok()
+                .body(taskService.getAllTasks());
+    }
+
+    @PatchMapping(path = "/{taskId}")
+    public ResponseEntity<TaskDto> updateTaskById(@PathVariable(name = "taskId")
+                                                  Long taskId,
+                                                  @RequestBody
+                                                  TaskDto taskDto) {
+        return ResponseEntity.ok()
+                .body(taskService.updateTaskById(taskId, taskDto)); //TODO разобрать необходимость taskId
+
+    }
+
+    @DeleteMapping(path = "/{taskId}")
+    public ResponseEntity<Void> deleteTaskById(@PathVariable(name = "taskId") Long taskId) {
+
+        taskService.deleteTaskById(taskId);
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
--- src/main/java/antonfeklichev/tasktrackerapp/service/TaskService.java
+++ src/main/java/antonfeklichev/tasktrackerapp/service/TaskService.java
@@ -1,4 +1,21 @@
 package antonfeklichev.tasktrackerapp.service;
 
+import antonfeklichev.tasktrackerapp.dto.NewTaskDto;
+import antonfeklichev.tasktrackerapp.dto.TaskDto;
+
+import java.util.List;
+
 public interface TaskService {
+
+    TaskDto addTask(NewTaskDto createTaskDto);
+
+    TaskDto getTaskById(Long taskId);
+
+    List<TaskDto> getAllTasks();
+
+    TaskDto updateTaskById(Long taskId, TaskDto taskDto);
+
+    void deleteTaskById(Long taskId); //TODO удалять подзадачи или кидать исключение
+
+
 }
