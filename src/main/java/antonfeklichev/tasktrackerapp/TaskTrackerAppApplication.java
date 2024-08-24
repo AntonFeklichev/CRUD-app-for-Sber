547e0b3bf914b03928e600cb7532c07df50e1e01
Anton <afeklichev@yandex.ru>
2024-08-24 14:07:23 +0300

    add exceptions

--- src/main/java/antonfeklichev/tasktrackerapp/TaskTrackerAppApplication.java
+++ src/main/java/antonfeklichev/tasktrackerapp/TaskTrackerAppApplication.java
@@ -2,22 +2,12 @@ package antonfeklichev.tasktrackerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.util.Streamable;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class TaskTrackerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerAppApplication.class, args);
-
 	}
-
-
-
-
 
 }
