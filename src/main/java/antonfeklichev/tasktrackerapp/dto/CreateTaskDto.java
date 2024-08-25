791d7efc2d509ba74a379d4bf76b9e8fbfb43288
Anton <afeklichev@yandex.ru>
2024-08-25 15:00:20 +0300

    add javadoc

--- src/main/java/antonfeklichev/tasktrackerapp/dto/CreateTaskDto.java
+++
@@ -1,6 +0,0 @@
-package antonfeklichev.tasktrackerapp.dto;
-
-import antonfeklichev.tasktrackerapp.entity.TaskStatus;
-
-public record CreateTaskDto(String name, String description, TaskStatus status) {
-}
--- src/main/java/antonfeklichev/tasktrackerapp/dto/TaskFilterDto.java
+++
@@ -1,6 +0,0 @@
-package antonfeklichev.tasktrackerapp.dto;
-
-import antonfeklichev.tasktrackerapp.entity.TaskStatus;
-
-public record TaskFilterDto(TaskStatus status, String name) {
-}
